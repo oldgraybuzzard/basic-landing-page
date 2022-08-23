68ff29d6d00a53b72ba1010960db9672dd772f70
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-08-23 05:08:00 -0400

    all questions works

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const inquirer = require('inquirer');
 const { writeFile, copyFolder } = require('./utils/generate-site.js');
 const generateLandingPage = require('./src/page-template');
 
-const promptUser = () => {
+const promptSetupQuestions = () => {
     return inquirer.prompt([
+        // =========Header Section==============        
         {
             type: 'input',
-            name: 'name',
+            name: 'siteName',
             message: 'What is the name of the site? (Required)',
             validate: nameInput => {
                 if (nameInput) {
@@ -19,8 +20,16 @@ const promptUser = () => {
         },
         {
             type: 'input',
-            name: 'link',
-            message: 'What is the address to your website?'
+            name: 'siteLink',
+            message: 'What is the address to your website?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the URL to the site!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
@@ -41,25 +50,12 @@ const promptUser = () => {
                 }
             }
         },
-        {
-            type: 'input',
-            name: 'header',
-            message: 'What is the header text?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter Header text!');
-                    return false;
-                }
-            }
-
-        },
+        // =========Benefit Section==============        
         {
             type: 'confirm',
             name: 'confirmBenefit',
             message: 'Is there woridng to add in the hero section?',
-            default: true
+            default: false
         },
         {
             type: 'input',
@@ -74,6 +70,7 @@ const promptUser = () => {
                 }
             }
         },
+        // =========Email Form==============        
         {
             type: 'confirm',
             name: 'confirmForm',
@@ -94,21 +91,332 @@ const promptUser = () => {
         },
         {
             type: 'input',
-            name: 'emailSubjest',
+            name: 'emailSubject',
             message: 'What will be the subject of your email?',
-            
-
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the subject of the email!');
+                    return false;
+                }
+            }
         },
         {
+            type: 'input',
+            name: 'emailBody',
+            message: 'What is the body of the email?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the text to go in the body of the email!');
+                    return false;
+                }
+            }
+        },
+        // =========Feature Section==============        
+        {
             type: 'confirm',
-            name: 'addSocialMedia',
+            name: 'confirmFeature',
+            message: 'Do you want to add in a feature section?',
+            default: false  
+        },
+        // =========Feature One==============
+        {
+            type: 'input',
+            name: 'featureOneIcon',
+            message: 'What icon to use for feature one?',
+            when: ({confirmFeature}) => {
+                if ({confirmFeature}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'featureOneHeader',
+            message: 'What is feature one header text?',
+            when: ({confirmFeature}) => {
+                if ({confirmFeature}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'featureOneText',
+            message: 'What is feature one text?',
+            when: ({confirmFeature}) => {
+                if ({confirmFeature}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        // =========Feature Two==============                
+        {
+            type: 'input',
+            name: 'featureTwoIcon',
+            message: 'What icon to use for feature two?',
+            when: ({confirmFeature}) => {
+                if ({confirmFeature}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+                {
+            type: 'input',
+            name: 'featureTwoHeader',
+            message: 'What is feature two header text?',
+            when: ({confirmFeature}) => {
+                if ({confirmFeature}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'featureTwoText',
+            message: 'What is feature two text?',
+            when: ({confirmFeature}) => {
+                if ({confirmFeature}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        // =========Feature Three==============
+        {
+            type: 'input',
+            name: 'featureThreeIcon',
+            message: 'What icon to use for feature three?',
+            when: ({confirmFeature}) => {
+                if ({confirmFeature}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'featureThreeHeader',
+            message: 'What is feature three header text?',
+            when: ({confirmFeature}) => {
+                if ({confirmFeature}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'featureThreeText',
+            message: 'What is feature three text?',
+            when: ({confirmFeature}) => {
+                if ({confirmFeature}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        // =========Social Media==============                        
+        {
+            type: 'confirm',
+            name: 'confirmSocialMedia',
             message: 'Add Social Media links?',
             default: false
+        },
+        // =========Facebook==============            
+        {
+            type: 'confirm',
+            name: 'addFacebook',
+            message: 'Add a Facebook accout?',
+            default: false,
+            when: ({confirmSocialMedia}) => {
+                if ({confirmSocialMedia}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'facebook',
+            message: 'What is the facebook link?',
+            when: ({addFacebook}) => {
+                if ({addFacebook}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        // =========Twitter==============   
+        {
+            type: 'confirm',
+            name: 'addTwitter',
+            message: 'Add a Twitter accout?',
+            default: false,
+            when: ({confirmSocialMedia}) => {
+                if ({confirmSocialMedia}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'twitter',
+            message: 'What is the twitter link?',
+            when: ({addTwitter}) => {
+                if ({addTwitter}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        // =========Instagram==============   
+        {
+            type: 'confirm',
+            name: 'addInstagram',
+            message: 'Add an Instagram accout?',
+            default: false,
+            when: ({confirmSocialMedia}) => {
+                if ({confirmSocialMedia}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'instagram',
+            message: 'What is the instagram link?',
+            when: ({addInstagram}) => {
+                if ({addInstagram}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        // =========Snapchat==============   
+        {
+            type: 'confirm',
+            name: 'addSnapchat',
+            message: 'Add a Snapchat accout?',
+            default: false,
+            when: ({confirmSocialMedia}) => {
+                if ({confirmSocialMedia}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'snapchat',
+            message: 'What is the snapchat link?',
+            when: ({addSnapchat}) => {
+                if ({addSnapchat}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        // =========WhatsApp==============   
+        {
+            type: 'confirm',
+            name: 'addWhatsapp',
+            message: 'Add a WhatsApp accout?',
+            default: false,
+            when: ({confirmSocialMedia}) => {
+                if ({confirmSocialMedia}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'whatsapp',
+            message: 'What is the WhatsApp link?',
+            when: ({addWhatsapp}) => {
+                if ({addWhatsapp}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        // =========Footer==============
+        {
+            type: 'confirm',
+            name: 'confirmFooter',
+            message: 'Add in footer section?',
+            default: true                
+        },
+        {
+            type: 'input',
+            name: 'aboutLink',
+            message: 'What is the link to the main site About?',
+            when: ({confirmFooter}) => {
+                if ({confirmFooter}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'termsLink',
+            message: 'What is the link to the main site Terms of Use?',
+            when: ({confirmFooter}) => {
+                if ({confirmFooter}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'privacyLink',
+            message: 'What is the link to the main site Privacy Policy?',
+            when: ({confirmFooter}) => {
+                if ({confirmFooter}) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         },
     ]);
 };
 
-promptUser()
+promptSetupQuestions()
   .then(landingPageInfo => {
       return generateLandingPage(landingPageInfo);
   })    
--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,10 @@
 module.exports = templateData => {
+  //set current year
+  function setYear() {
+  document.write(new Date().getFullYear())
+}
+  
+  
   // destructure projects and about data from templateData based on their property key names
     const { link, slogan, benefit, ...header } = templateData;
 
@@ -12,9 +18,9 @@ module.exports = templateData => {
         <meta name="author" content="" />
         <title>${header.name} Landing Page</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
@@ -24,7 +30,8 @@ module.exports = templateData => {
         <!-- Navigation-->
         <nav class="navbar navbar-light bg-light static-top">
             <div class="container">
-                <a class="navbar-brand" href="${link}">${slogan}</a>
+                <a class="navbar-brand" href="${siteLink}">${header.name}</a>
+                <a class="navbar-text">${slogan}</a>
             </div>
         </nav>
         <!-- Masthead-->
@@ -46,23 +53,23 @@ module.exports = templateData => {
                 <div class="row">
                     <div class="col-lg-4">
                         <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-window m-auto text-primary"></i></div>
-                            <h3>Fully Responsive</h3>
-                            <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
+                            <div class="features-icons-icon d-flex"><i class="${featureOneIcon} m-auto text-primary"></i></div>
+                            <h3>${featureOneHeader}</h3>
+                            <p class="lead mb-0">${featureOneText}</p>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
-                            <h3>Bootstrap 5 Ready</h3>
-                            <p class="lead mb-0">Featuring the latest build of the new Bootstrap 5 framework!</p>
+                            <div class="features-icons-icon d-flex"><i class="${featureTwoIcon} m-auto text-primary"></i></div>
+                            <h3>${featureTwoHeader}</h3>
+                            <p class="lead mb-0">${featureTwoText}</p>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-terminal m-auto text-primary"></i></div>
-                            <h3>Easy to Use</h3>
-                            <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+                            <div class="features-icons-icon d-flex"><i class="${featureThreeIcon} m-auto text-primary"></i></div>
+                            <h3>${featureThreeHeader}</h3>
+                            <p class="lead mb-0">${featureThreeText}</p>
                         </div>
                     </div>
                 </div>
@@ -74,7 +81,7 @@ module.exports = templateData => {
                 <div class="row justify-content-center">
                     <div class="col-xl-6">
                         <h2 class="mb-4">Ready to get started? Contact up now!</h2>
-                        <form class="form-subscribe" id="subscribeForm" action="mailto:${formEmail}?subject=We want to sign up&body=Sign us up" method="POST" enctype="multipart/form-data" name="emailForm">
+                        <form class="form-subscribe" id="subscribeForm" action="mailto:${formEmail}?subject=${emailSubject}&body=${emailBody}" method="POST" enctype="multipart/form-data" name="emailForm">
                             <div class="row">
                                 <div class="col">
                                     <input type="email" class="form-control form-control-lg" id="emailAddress" placeholder="Enter Your Email Address" aria-describedby="emailHelp">
@@ -94,34 +101,43 @@ module.exports = templateData => {
                 <div class="row">
                     <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
                         <ul class="list-inline mb-2">
-                            <li class="list-inline-item"><a href="#!">About</a></li>
+                            <li class="list-inline-item"><a href="${aboutLink}">About</a></li>
                             <li class="list-inline-item">⋅</li>
-                            <li class="list-inline-item"><a href="#!">Contact</a></li>
+                            <li class="list-inline-item"><a href="${termsLink}">Terms of Use</a></li>
                             <li class="list-inline-item">⋅</li>
-                            <li class="list-inline-item"><a href="#!">Terms of Use</a></li>
-                            <li class="list-inline-item">⋅</li>
-                            <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
+                            <li class="list-inline-item"><a href="${privacyLink}">Privacy Policy</a></li>
                         </ul>
-                        <p class="text-muted small mb-4 mb-lg-0">&copy; ${header.name} 2022. All Rights Reserved.</p>
+                        <p class="text-muted small mb-4 mb-lg-0">&copy; ${siteName} ${setYear()}. All Rights Reserved.</p>
                     </div>
                     <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
                         <ul class="list-inline mb-0">
                             <li class="list-inline-item me-4">
-                                <a href="#!"><i class="bi-facebook fs-3"></i></a>
+                                <a href="${facebook}"><i class="bi-facebook fs-3"></i></a>
                             </li>
                             <li class="list-inline-item me-4">
-                                <a href="#!"><i class="bi-twitter fs-3"></i></a>
+                                <a href="${twitter}"><i class="bi-twitter fs-3"></i></a>
                             </li>
-                            <li class="list-inline-item">
-                                <a href="#!"><i class="bi-instagram fs-3"></i></a>
+                            <li class="list-inline-item me-4">
+                                <a href="${instagram}"><i class="bi-instagram fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item me-4">
+                                <a href=${snapchat}"><i class="bi bi-snapchat fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item me-4">
+                                <a href="${whatsapp}"><i class="bi bi-whatsapp fs-3"></i></a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
         </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+        </script>
+        // reserve
+        <!-- Reserved for future update
         <script src="assets/script.js"></script>
+        -->
     </body>
   </html>
   `;
